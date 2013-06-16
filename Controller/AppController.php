febb06cd382e726b9c424ce9153aa73993532573
Victor Yap <starlocke@gmail.com>
2013-06-15 22:55:33 -0400

    Added RequestHandler switch to JsonView

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -32,4 +32,11 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+  public $components = array('RequestHandler');
+  
+  public function beforeFilter(){
+    if ($this->RequestHandler->accepts('json')) {
+      $this->viewClass = 'Json';
+    }
+  }
 }
