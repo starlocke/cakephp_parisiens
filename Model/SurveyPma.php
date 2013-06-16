6bacaa8d03f7dce02be71e0d3dd8e8305f1648fe
Victor Yap <starlocke@gmail.com>
2013-06-15 20:46:40 -0400

    Baked models

---
+++ Model/SurveyPma.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * SurveyPma Model
+ *
+ */
+class SurveyPma extends AppModel {
+
+/**
+ * Use table
+ *
+ * @var mixed False or table name
+ */
+	public $useTable = 'survey_pma';
+
+}
---
+++ Model/SurveySm.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * SurveySm Model
+ *
+ */
+class SurveySm extends AppModel {
+
+/**
+ * Use table
+ *
+ * @var mixed False or table name
+ */
+	public $useTable = 'survey_sm';
+
+}
